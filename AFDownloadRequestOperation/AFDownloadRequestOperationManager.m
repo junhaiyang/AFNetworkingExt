239ef8aaa4b0896dc21e63bbbe07c0986672aeab
yangjunhai <junhaiyang@gmail.com>
2014-07-26 13:09:51 +0800

    修改

--- AFDownloadRequestOperation/AFDownloadRequestOperationManager.m
+++ AFDownloadRequestOperation/AFDownloadRequestOperationManager.m
@@ -8,6 +8,16 @@
 
 #import "AFDownloadRequestOperationManager.h"
 #import "AFDownloadRequestOperation.h"
+
+static dispatch_queue_t download_request_operation_completion_queue() {
+    static dispatch_queue_t af_url_request_operation_completion_queue;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        af_url_request_operation_completion_queue = dispatch_queue_create("com.soooner.networking.operation.queue", DISPATCH_QUEUE_CONCURRENT );
+    });
+    
+    return af_url_request_operation_completion_queue;
+}
  
 @implementation AFDownloadRequestOperationManager
 
@@ -20,6 +30,10 @@
     operation.shouldUseCredentialStorage = self.shouldUseCredentialStorage;
     operation.credential = self.credential;
     operation.securityPolicy = self.securityPolicy;
+    
+    
+    if(!self.asyncwork)
+        operation.completionQueue = download_request_operation_completion_queue();
     
     [operation setCompletionBlockWithSuccess:success failure:failure];
     
